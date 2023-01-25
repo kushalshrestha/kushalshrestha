38a7e43d703207bdc2203208c0ded60729cb00f4
kushalshrestha <kushalshr@gmail.com>
2023-01-25 04:59:07 -0600

    feat: Scope exercises

---
+++ js/pdf-output.js
@@ -0,0 +1,24 @@
+// var count = (function () {
+//   var counter = 0;
+//   return {
+//     add: function () {
+//       return (counter += 1);
+//     },
+//     reset: function () {
+//       return (counter = 0);
+//     },
+//     make_adder: function (inc) {
+//       return (counter += inc);
+//     },
+//   };
+// })();
+
+// add5 = count.make_adder(5);
+// console.log(add5);
+// add5();
+// add5();
+// add5(); // final counter value is 15
+// add7 = make_adder(7);
+// add7();
+// add7();
+// add7(); // final counter value is 21
